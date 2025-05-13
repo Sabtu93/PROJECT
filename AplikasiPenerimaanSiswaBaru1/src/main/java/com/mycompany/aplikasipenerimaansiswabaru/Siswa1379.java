5895e1908750e1e462c7d2cbbb923241445e1ace
Sabtu93 <muhdrifqifauzan@gmail.com>
2025-05-13 15:30:37 +0700

    Add files via upload

---
+++ AplikasiPenerimaanSiswaBaru1/src/main/java/com/mycompany/aplikasipenerimaansiswabaru/Siswa1379.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aplikasipenerimaansiswabaru;
+
+/**
+ *
+ * @author RIFQI
+ */
+
+public class Siswa1379 {
+    private String nama1379;
+    private String nisn1379;
+    private String asal1379;
+    private int nilai1379;
+
+    public Siswa1379(String nama1379, String nisn1379, String asal1379, int nilai1379) {
+        this.nama1379 = nama1379;
+        this.nisn1379 = nisn1379;
+        this.asal1379 = asal1379;
+        this.nilai1379 = nilai1379;
+    }
+
+    public String getNama1379() { return nama1379; }
+    public String getNisn1379() { return nisn1379; }
+    public String getAsal1379() { return asal1379; }
+    public int getNilai1379() { return nilai1379; }
+
+    // 🔧 Tambahkan ini untuk mendukung fitur EDIT
+    public void setNama1379(String nama1379) { this.nama1379 = nama1379; }
+    public void setNisn1379(String nisn1379) { this.nisn1379 = nisn1379; }
+    public void setAsal1379(String asal1379) { this.asal1379 = asal1379; }
+    public void setNilai1379(int nilai1379) { this.nilai1379 = nilai1379; }
+}
+
